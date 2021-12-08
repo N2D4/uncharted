47020361dfce4ac41d052ddf8db0f9e6ed8b85e2
Stan Wohlwend <n2d4xc@gmail.com>
2021-12-08 02:30:06 +0100

    Remove dead code

--- src/components/result-viewers/ChartResultViewer.svelte
+++ src/components/result-viewers/ChartResultViewer.svelte
@@ -60,49 +60,6 @@ import { stringify } from 'uuid';
 		}
 
 		return { error: null, parameter: computeParameter, data };
-
-		header = [computeParameters.map((x) => x[0]), results];
-
-		// Rows
-		const valuesPerParam =
-			computeParameters.length === 0
-				? 1
-				: Math.min(10, Math.max(2, Math.floor(20 ** (1 / computeParameters.length))));
-		const totalRows = valuesPerParam ** computeParameters.length;
-		rows = new Array(totalRows).fill(0).map(() => [[], []]);
-		for (let i = 0; i < totalRows; i++) {
-			let j = i;
-			const assignedParameters = new Map<Parameter, number>();
-			for (const [parameter, value] of computeParameters) {
-				const [_, min, max] = value;
-				const step = (max - min) / (valuesPerParam - 1);
-				const assignedValue = min + (j % valuesPerParam) * step;
-				assignedParameters.set(parameter, assignedValue);
-				rows[i][0].push(prettyToString(assignedValue));
-				j = Math.floor(j / valuesPerParam);
-			}
-
-			const args = [...parameters].map(([parameter, value]) => {
-				switch (value[0]) {
-					case 'number-range':
-						return assignedParameters.get(parameter)!;
-					case 'literal':
-						return value[1];
-					default:
-						throw new Error(`Unknown parameter type ${value[0]}`);
-				}
-			});
-			try {
-				const value = func(...args);
-
-				for (const result of results) {
-					rows[i][1].push(prettyToString(value?.[result.name]));
-				}
-			} catch (e) {
-				console.error(`Error evaluating table`, { parameters, error: e, func });
-				rows[i][1].push(e instanceof Error ? e.name : `error: ${e}`);
-			}
-		}
 	})();
 </script>
 
